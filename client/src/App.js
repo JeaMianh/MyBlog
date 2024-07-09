078c91bf8295ec0c6226b492a51f9e99359a0a41
JeaMianh <JeaMianh@foxmail.com>
2024-07-09 17:15:54 +0800

    client init

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Container, } from "react-bootstrap"
+import React from "react";
+
+function App() {
+  return (
+    <Container>
+      <h1> 你好 </h1>
+    </Container>
+  );
+}
+
+export default App;
